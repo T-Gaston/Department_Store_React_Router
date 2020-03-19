fc561f2f7dfef5b4870a40a4a3b444431bbc4f3c
Thomas Gaston <thomasgaston11@gmail.com>
2020-03-19 09:35:02 -0600

    whatever

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     resources :departments
   end
+
+  resources :departments do
+    resources :items
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
